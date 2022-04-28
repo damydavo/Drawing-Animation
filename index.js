1d5c642a19faa8f9931831aa58b09b5b6f1016e0
unknown <damydavo2015@gmail.com>
2022-04-28 18:22:28 +0100

    added the circle animation properties

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
+         if(callbacks) {
+          this.onStart = callbacks.onStart
+          this.onTick = callbacks.onTick
+          this.onComplete = callbacks.onComplete
+        }
 
         this.startButton.addEventListener('click', this.start)
         this.pauseButton.addEventListener('click', this.pause)
   }
 
   start = () => {
+    if(this.onStart) {
+        this.onStart()
+    }
+
       this.tick()
-      this.intervalId = setInterval(this.tick, 1000)
+      this.intervalId = setInterval(this.tick, 50)
   } 
 
   tick = () => {
+    if(this.timeRemaining <= 0) {
+      this.pause()
+      if(this.onComplete) {
+           this.onComplete();
+      }
+    } else {
+     this.timeRemaining = this.timeRemaining - 0.05 
+     if(this.onTick) {
+         this.onTick()
+     }
 
-    this.timeRemaining <= 0 ? this.pause() : this.timeRemaining = this.timeRemaining - 1
+    }
+
   } 
 
     get timeRemaining() {
@@ -35,5 +55,24 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const circle = document.querySelector('circle')
+const perimeter = circle.getAttribute('r') * 2 * Math.PI
+circle.setAttribute('stroke-dasharray', perimeter)
+
+
+let currentOffset = 0;
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log('Timer has just started')
+  },
+
+  onTick() {
+    circle.setAttribute('stroke-dashoffset', currentOffset)
+    currentOffset = currentOffset - 1
+  },
+
+  onComplete() {
+    console.log("Timer was completed")
+  }
+});
 
